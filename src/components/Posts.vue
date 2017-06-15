997210f0e25c76367d17014e69b867335c9923c7
Savannah Barber <savannahlee117@gmail.com>
2017-06-15 11:08:23 -0400

    Example thumbnail post on main page

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,10 +1,22 @@
 <template>
   <!-- This is where data should be retrieved from the DB and a v-for directive is used to iterate over the data -->
+  <div class="row">
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="../assets/logo.png" alt="...">
+        <div class="caption">
+          <h3>Thumbnail Label</h3>
+          <p>Description goes here</p>
+          <p><a href="#" class="btn btn-primary" role="button">View Post</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
 
