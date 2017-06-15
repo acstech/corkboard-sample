86d81521464778ae8ee6399e2c0004866a204e35
Savannah Barber <savannahlee117@gmail.com>
2017-06-15 11:19:42 -0400

    Thumbnail example adjustments

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,13 +1,14 @@
 <template>
   <!-- This is where data should be retrieved from the DB and a v-for directive is used to iterate over the data -->
   <div class="row">
-    <div class="col-sm-6 col-md-4">
+    <div class="col-sm-6 col-md-4"> <!-- v-for? -->
       <div class="thumbnail">
         <img src="../assets/logo.png" alt="...">
         <div class="caption">
           <h3>Thumbnail Label</h3>
+          <h4>Price</h4>
           <p>Description goes here</p>
-          <p><a href="#" class="btn btn-primary" role="button">View Post</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+          <p><a href="#" class="btn btn-primary" role="button">View Post</a> <a href="#" class="btn btn-default" role="button">Contact Seller</a></p>
         </div>
       </div>
     </div>
