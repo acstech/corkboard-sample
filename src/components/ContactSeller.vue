c097c660be6fc0bf381ef9a888e8a84606a37817
dan108 <dan108@msstate.edu>
2017-07-07 15:23:46 -0400

    multiple changes, including a contact seller file

---
+++ src/components/ContactSeller.vue
@@ -0,0 +1,31 @@
+<template>
+  <transition name="modal">
+    <post-modal>
+      <div class="modal-header">
+        <router-link class="close" to="/">&times;</router-link>
+      </div>
+
+      <div class="modal-body">
+        UGH
+      </div>
+
+      <div class="modal-footer">
+
+      </div>
+    </post-modal>
+  </transition>
+</template>
+
+<script>
+
+import PostModal from './PostModal.vue'
+export default {
+  components: {
+    postModal: PostModal
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
