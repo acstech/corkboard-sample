8b72d06fab4b95bf1cefe5ab13ac7ddb2f646567
Savannah Barber <savannahlee117@gmail.com>
2017-07-07 07:50:49 -0400

    Continued form optimization

--- src/components/EditProfile.vue
+++ src/components/EditProfile.vue
@@ -107,7 +107,7 @@ export default {
     },
     numberPressed (evt) {
       var charCode = (evt.which) ? evt.which : evt.keyCode
-      if (charCode > 31 && (charCode < 48 || charCode > 57) && (charCode < 36 || charCode > 40)) {
+      if ((charCode < 48 || charCode > 57)) {
         evt.preventDefault()
         return false
       }
